64f7af233041d90f08099f81a1568c13d9fe7f30
rensicheng <472979947@qq.com>
2019-01-05 00:52:12 +0800

    finish

--- products.py
+++ products.py
@@ -6,3 +6,10 @@ while True:
 	price = input('请输入商品价格：')
 	products.append([name, price])
 print(products)
+for product in products:
+	print(product)
+
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品, 价格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
